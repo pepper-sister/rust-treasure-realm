74a079882739eb7cd0ac350c8fe0767b87c2d566
pepper-sister <yuyu7123@naver.com>
2025-11-15 06:07:44 +0900

    feat: 최소 금액 error

--- src/functions/get_purchase_amount.rs
+++ src/functions/get_purchase_amount.rs
@@ -1,10 +1,13 @@
 use crate::functions::read_input::read_input;
+use crate::constants::other_constants::MIN_PURCHASE_AMOUNT;
 
 pub fn get_purchase_amount() -> u32 {
   loop {
-    if let Ok(input) = read_input("뽑기를 진행할 금액을 입력해 주세요.") {
-    if let Ok(amount) = input.parse::<u32>() {
-      return amount;
+    if let Ok(input) = read_input("\n뽑기를 진행할 금액을 입력해 주세요.") {
+      match input.parse::<u32>() {
+        Ok (amount) if amount >= MIN_PURCHASE_AMOUNT as u32 => return amount,
+        Ok(_) => println!("[ERROR] 구입 금액은 최소 {}원 이상 입력해야 합니다.", MIN_PURCHASE_AMOUNT),
+        Err(_) => println!("[ERROR] 올바른 숫자 형식이 아닙니다."),
       }
     }
   }
