69c6079536c7a874e2072bd3f224a5a166716869
pepper-sister <yuyu7123@naver.com>
2025-11-15 06:10:54 +0900

    chore: 탭 크기 수정

--- src/functions/amount_to_coins.rs
+++ src/functions/amount_to_coins.rs
@@ -3,8 +3,8 @@ use crate::data::state::Player;
 pub fn amount_to_coins(player: &mut Player, mut amount: u32, tft_coin: &[(u32, u32)]) {
   for (price, coins) in tft_coin.iter() {
     while amount >= *price {
-        player.tft_coin += coins;
-        amount -= *price;
+      player.tft_coin += coins;
+      amount -= *price;
     }
   }
 }
