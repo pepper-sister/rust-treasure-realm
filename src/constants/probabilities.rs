f9f2bbf50edf883c11c21295bf25888a69e510d9
pepper-sister <yuyu7123@naver.com>
2025-11-15 04:33:36 +0900

    chore: 탭 크기 수정

--- src/constants/probabilities.rs
+++ src/constants/probabilities.rs
@@ -1,10 +1,12 @@
+use crate::data::rewards::RewardType;
+
 pub const PROBABILITIES_MAX: u32 = 10000000;
 
 pub const PROBABILITIES: &[(RewardType, u32)] = &[
-    (RewardType::Prestige, 1000),
-    (RewardType::Myth, 30000),
-    (RewardType::Legend, 500000),
-    (RewardType::Common, 4000000),
-    (RewardType::KingdomCrystal, 5438995),
-    (RewardType::MythMedal, 30005),
+  (RewardType::Prestige, 1000),
+  (RewardType::Myth, 30000),
+  (RewardType::Legend, 500000),
+  (RewardType::Common, 4000000),
+  (RewardType::KingdomCrystal, 5438995),
+  (RewardType::MythMedal, 30005),
 ];
--- src/data/rewards.rs
+++ src/data/rewards.rs
@@ -8,10 +8,10 @@ pub enum RewardType {
 }
 
 pub const REWARDS_NAMES: &[(RewardType, &'static str)] = &[
-    (RewardType::Prestige, "[프레스티지]미니 영혼의 꽃 아리 🌟"),
-    (RewardType::Myth, "[신화]미니 우주 그루브 블리츠크랭크 🥊"),
-    (RewardType::Legend, "[전설]형님 샥샥이 🦈"),
-    (RewardType::Common, "[일반]울끈불끈 집게발 🦞"),
-    (RewardType::KingdomCrystal, "왕국 수정 🔮"),
-    (RewardType::MythMedal, "신화 메달 🎖️"),
+  (RewardType::Prestige, "[프레스티지]미니 영혼의 꽃 아리 🌟"),
+  (RewardType::Myth, "[신화]미니 우주 그루브 블리츠크랭크 🥊"),
+  (RewardType::Legend, "[전설]형님 샥샥이 🦈"),
+  (RewardType::Common, "[일반]울끈불끈 집게발 🦞"),
+  (RewardType::KingdomCrystal, "왕국 수정 🔮"),
+  (RewardType::MythMedal, "신화 메달 🎖️"),
 ];
